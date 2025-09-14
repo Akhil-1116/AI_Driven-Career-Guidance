cdef12e169d950f6ed0d0145ecb7e4202309531a
Akhil <princeakhil195@gmail.com>
2025-09-14 21:21:51 +0530

    Updated routes.py with necessary changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,22 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+from dotenv import load_dotenv
 import os
 
+load_dotenv()  # Load variables from .env file
 
-mongo = PyMongo()  # Create mongo outside so we can init it later
+mongo = PyMongo()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.urandom(24)
+    
+    # Correct usage of environment variable
     app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-
-    # Initialize MongoDB with app
+    
     mongo.init_app(app)
-
-    # Register Blueprints
-    from app.routes import auth  # Make sure your blueprint is in routes.py
+    
+    from app.routes import auth
     app.register_blueprint(auth)
 
     return app
-
-
-
